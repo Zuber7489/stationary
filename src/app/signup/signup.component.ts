52990f45cd5a8ec90bd24edd0ddf20f7294c0f45
Zuber7489 <zs808081@gmail.com>
2022-08-25 11:15:11 +0530

    changes

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 @Component({
@@ -8,24 +8,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  
 
-  constructor(public http:HttpClient, public  route:Router) { }
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  constructor(public http: HttpClient, public route: Router) { }
 
   ngOnInit(): void {
   }
 
   signupForm = new FormGroup({
-    name: new FormControl('',Validators.required),
-    email: new FormControl('',Validators.required),
-    number: new FormControl('',Validators.required),
-    
-    password: new FormControl('',Validators.required)
+    name: new FormControl('', Validators.minLength(8)),
+    email: new FormControl('', Validators.pattern(this.emailPattern)),
+    number: new FormControl('', Validators.required),
+
+    password: new FormControl('', Validators.required)
   })
 
+  get name() {
+    return this.signupForm.get('name');
+  }
+  get email() {
+    return this.signupForm.get('email');
+  }
 
-  signUp(){
-    this.http.post('http://localhost:3000/posts',this.signupForm.value).subscribe(res=>{
+  signUp() {
+    this.http.post('http://localhost:3000/posts', this.signupForm.value).subscribe(res => {
       alert("Student Registered Successfully");
       this.signupForm.reset();
       this.route.navigate(['login'])
